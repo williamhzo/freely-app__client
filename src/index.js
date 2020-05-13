4be6911ae95ef52acd33311e06692520c400e21a
William Hermozo <william.hermozo@protonmail.com>
2020-05-13 19:48:17 +0200

    remove style reset

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import UserProvider from "./components/Auth/UserProvider";
 import App from "./App";
 
-import "./styles/reset.scss";
 import "./styles/global.scss";
 
 ReactDOM.render(
