a7bd26e9c22aaec00826d24441df3707fdd6bf0d
William Hermozo <william.hermozo@protonmail.com>
2020-05-14 17:49:22 +0200

    add icons to nav bar

--- src/components/Forms/FormSignin.jsx
+++ src/components/Forms/FormSignin.jsx
@@ -1,28 +1,19 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import UserContext from "../Auth/UserContext";
-import { withRouter } from "react-router-dom";
-import apiHandler from "../../api/apiHandler";
+import UserContext from '../Auth/UserContext';
+import { withRouter } from 'react-router-dom';
+import apiHandler from '../../api/apiHandler';
 
 class FormSignin extends Component {
   static contextType = UserContext;
 
   state = {
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   };
 
   handleChange = (event) => {
-    const key = event.target.name;
-
-    // You can test more if you have to handle different sorts of inputs.
-    const value =
-      event.target.type === "file"
-        ? event.target.files[0]
-        : event.target.type === "checkbox"
-        ? event.target.checked
-        : event.target.value;
-
+    const { key, value } = event.target;
     this.setState({ [key]: value });
   };
 
@@ -33,11 +24,11 @@ class FormSignin extends Component {
       .signin(this.state)
       .then((data) => {
         this.context.setUser(data);
-        this.props.history.push("/");
+        this.props.history.push('/');
       })
       .catch((error) => {
         console.log(error);
-        // Display error message here, if you set the state
+        // form validation !!!
       });
   };
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { withUser } from './Auth/withUser';
 import apiHandler from '../api/apiHandler';
+import UserContext from './Auth/UserContext';
+
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import '../styles/NavBar.scss';
 
@@ -20,47 +24,72 @@ const NavBar = (props) => {
   }
 
   return (
-    <nav className="Nav">
-      <NavLink className="Nav__link" exact to="/">
-        <h3 className="Nav__logo">
-          Shark <br></br> Attack
-        </h3>
-        <span role="img" aria-label="shark-emoji">
-          🦈
-        </span>
-      </NavLink>
-      <ul className="Nav__list">
-        <li className="Nav__item">
-          <NavLink className="Nav__link" to="/profile">
-            <img src="media/avatar.png" alt="avatar" />
+    <UserContext.Consumer>
+      {(context) => (
+        <nav className="Nav">
+          <NavLink className="Nav__link" exact to="/">
+            <h3 className="Nav__logo">
+              Shark <br></br> Attack
+            </h3>
+            <span role="img" aria-label="shark-emoji">
+              🦈
+            </span>
           </NavLink>
-        </li>
-        {context.isLoggedIn && (
-          <React.Fragment>
+          <ul className="Nav__list">
             <li className="Nav__item">
-              <NavLink className="Nav__link" to="/profile">
-                {context.user && context.user.email}
+              <NavLink className="Nav__link" exact to="/collabs-create">
+                <img className="Nav__icon" src="media/plus-icon.svg" alt="+" />
               </NavLink>
             </li>
+            {context.user && (
+              <div className="Nav__avatar">
+                <li className="Nav__item">
+                  <NavLink className="Nav__link" to="/profile">
+                    <img src={context.user.profilePicture} alt="avatar" />
+                  </NavLink>
+                </li>
+              </div>
+            )}
+            {!context.user && (
+              <li className="Nav__item">
+                <div className="Nav__avatar">
+                  <NavLink className="Nav__link" to="/profile">
+                    <img src="media/avatar.png" alt="avatar" />
+                  </NavLink>
+                </div>
+              </li>
+            )}
             <li className="Nav__item">
-              <p onClick={handleLogout}>Logout</p>
+              <img className="Nav__icon" src="media/hamburger.svg" alt="menu" />
             </li>
-          </React.Fragment>
-        )}
-        {!context.isLoggedIn && (
-          <React.Fragment>
-            <li className="Nav__item">
-              <NavLink className="Nav__link" to="/signin">
-                Log in
-              </NavLink>
-            </li>
-            <li className="Nav__item">
-              <NavLink to="/signup">Create account</NavLink>
-            </li>
-          </React.Fragment>
-        )}
-      </ul>
-    </nav>
+            {/* {context.isLoggedIn && (
+              <React.Fragment>
+                <li className="Nav__item">
+                  <NavLink className="Nav__link" to="/profile">
+                    {context.user && context.user.email}
+                  </NavLink>
+                </li>
+                <li className="Nav__item">
+                  <p onClick={handleLogout}>Logout</p>
+                </li>
+              </React.Fragment>
+            )} */}
+            {/* {!context.isLoggedIn && (
+              <React.Fragment>
+                <li className="Nav__item">
+                  <NavLink className="Nav__link" to="/signin">
+                    Log in
+                  </NavLink>
+                </li>
+                <li className="Nav__item">
+                  <NavLink to="/signup">Create account</NavLink>
+                </li>
+              </React.Fragment>
+            )} */}
+          </ul>
+        </nav>
+      )}
+    </UserContext.Consumer>
   );
 };
 
