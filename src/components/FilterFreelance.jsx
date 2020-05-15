41517a64942de122bd961b47d2b80c5bd1351b0e
William Hermozo <william.hermozo@protonmail.com>
2020-05-15 17:57:06 +0200

    add profile cards on freelancer feed

--- src/components/FilterFreelance.jsx
+++ src/components/FilterFreelance.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const FilterFreelance = () => {
-  return <div></div>;
+  return (
+    <form className="filter-group" action="">
+      <label htmlFor="city">City</label>
+      <input name="city" id="city" type="text" />
+      <br></br>
+      <label htmlFor="category">Category</label>
+      <input type="text" name="category" id="category" />
+    </form>
+  );
 };
 
 export default FilterFreelance;
