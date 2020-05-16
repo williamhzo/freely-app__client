e75880cf1896e9d718b800254cf91bce26428526
William Hermozo <william.hermozo@protonmail.com>
2020-05-16 19:06:25 +0200

    add collabs feed page

--- src/components/FilterCollab.jsx
+++ src/components/FilterCollab.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const FilterCollab = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <form className="filter-group" action="">
+      <label htmlFor="city">City</label>
+      <input name="city" id="city" type="text" />
+      <br></br>
+      <label htmlFor="category">Category</label>
+      <input type="text" name="category" id="category" />
+    </form>
+  );
+};
 
-export default FilterCollab
+export default FilterCollab;
--- src/pages/CollabsFeed.jsx
+++ src/pages/CollabsFeed.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
+import Tabs from '../components/Nav/Tabs';
+import apiHandler from '../api/apiHandler';
+import FilterCollab from '../components/FilterCollab';
 
-const CollabsFeed = (props) => {
-  return (
-    <div>
-      <h1>Home Page ∆</h1>
-    </div>
-  );
-};
+import '../styles/FeedPage.scss';
+
+class CollabsFeed extends React.Component {
+  state = {
+    collabs: [],
+  };
+
+  componentDidMount() {
+    apiHandler
+      .getCollabs()
+      .then((apiRes) => this.setState({ collabs: apiRes }))
+      .catch((err) => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="feed-container">
+        <Tabs />
+        {!this.state.collabs && (
+          <h2>
+            Sorry, no collaborations found{' '}
+            <span role="img" aria-label="confused-face-emoji">
+              😕
+            </span>
+          </h2>
+        )}
+        <FilterCollab />
+        {this.state.collabs.map((collab, index) => (
+          <CollabCard key={index} collab={collab} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default CollabsFeed;
