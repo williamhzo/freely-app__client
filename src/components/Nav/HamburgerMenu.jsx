4f4f6320cfb1c7bd40223b217bb1943dec4da98c
William Hermozo <william.hermozo@protonmail.com>
2020-05-17 16:30:25 +0200

    refactor hamburger menu

--- src/components/Nav/HamburgerMenu.jsx
+++ src/components/Nav/HamburgerMenu.jsx
@@ -1,66 +1,90 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import apiHandler from '../../api/apiHandler';
-import UserContext from '../Auth/UserContext';
-import { Slant as Hamburger } from 'hamburger-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import apiHandler from '../../api/apiHandler';
+// import UserContext from '../Auth/UserContext';
+// import { Slant as Hamburger } from 'hamburger-react';
 
-class HamburgerMenu extends Component {
-  static contextType = UserContext;
+const HamburgerMenu = (props) => {
+  let hamburgerMenuClass = ['hamburger-menu'];
+  if (props.show) hamburgerMenuClass.push('open');
 
-  state = {
-    isToggled: false,
-  };
-
-  handleLogout = (event) => {
-    event.preventDefault();
-    apiHandler
-      .logout()
-      .then(() => {
-        this.context.removeUser();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    this.handleToggle();
-  };
-
-  handleToggle = (toggled) => {
-    this.setState({ isToggled: toggled ? true : false });
-  };
-
-  render() {
-    console.log(this.state.isToggled);
-    return (
-      <>
-        <Hamburger onToggle={this.handleToggle} />
-        {this.state.isToggled && (
-          <>
-            <div className="hamb-menu__background"></div>
-            <ul className="hamb-menu">
-              <li className="hamb-menu__item">
-                <NavLink
-                  onClick={this.handleToggle}
-                  to="/about"
-                  className="hamb-menu__item"
-                >
-                  About
-                </NavLink>
-              </li>
-              <li className="hamb-menu__item">
-                <NavLink
-                  onClick={this.handleLogout}
-                  to="/"
-                  className="hamb-menu__item"
-                >
-                  Log Out
-                </NavLink>
-              </li>
-            </ul>
-          </>
-        )}
-      </>
-    );
-  }
-}
+  console.log(props.show);
+  console.log(hamburgerMenuClass);
+  return (
+    <nav className={hamburgerMenuClass.join(' ')}>
+      <ul className="hamburger-menu__list">
+        <li className="hamburger-menu__item">
+          <Link className="hamburger-menu__link" to="/about">
+            About
+          </Link>
+        </li>
+        <li className="hamburger-menu__item">
+          <Link className="hamburger-menu__link" to="/">
+            Log Out
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default HamburgerMenu;
+
+// class HamburgerMenu extends Component {
+//   static contextType = UserContext;
+
+//   state = {
+//     isToggled: false,
+//   };
+
+//   handleLogout = (event) => {
+//     event.preventDefault();
+//     apiHandler
+//       .logout()
+//       .then(() => {
+//         this.context.removeUser();
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//     this.handleToggle();
+//   };
+
+//   handleToggle = (toggled) => {
+//     this.setState({ isToggled: toggled ? true : false });
+//   };
+
+//   render() {
+//     console.log(this.state.isToggled);
+//     return (
+//       <>
+//         <Hamburger onToggle={this.handleToggle} />
+//         {this.state.isToggled && (
+//           <>
+//             <div className="hamb-menu__background"></div>
+//             <ul className="hamb-menu">
+//               <li className="hamb-menu__item">
+//                 <NavLink
+//                   onClick={this.handleToggle}
+//                   to="/about"
+//                   className="hamb-menu__item"
+//                 >
+//                   About
+//                 </NavLink>
+//               </li>
+//               <li className="hamb-menu__item">
+//                 <NavLink
+//                   onClick={this.handleLogout}
+//                   to="/"
+//                   className="hamb-menu__item"
+//                 >
+//                   Log Out
+//                 </NavLink>
+//               </li>
+//             </ul>
+//           </>
+//         )}
+//       </>
+//     );
+//   }
+// }
