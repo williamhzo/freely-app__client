ebb0390d9931a6b30980b21a2f4cb42387637f80
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-05-18 16:50:31 +0200

    Add profile picture handling

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -8,8 +8,6 @@ import TextField from "@material-ui/core/TextField";
 import { TextareaAutosize } from "@material-ui/core";
 import { objectToFormData } from "object-to-formdata";
 import CityAutoComplete from "../components/CityAutoComplete";
-import CollabCard from "../components/Cards/CollabCard";
-import Axios from "axios";
 
 const checkLink = (link) => {
   let icon = "🌐";
@@ -44,9 +42,21 @@ export default class ProfileEdit extends Component {
       this.setState({ skillOptions: apiRes });
     });
   }
+  // fr.onload = function () {
+  //   document.getElementById(outImage).src = fr.result;
+  // }
+  // fr.readAsDataURL(files[0]);
   handleFormChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
     this.setState({ saved: false });
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.setState({ temporaryPicture: reader.result });
+    };
+    reader.readAsDataURL(e.target.files[0]);
+    if (e.target.name === "profilePicture") {
+      this.setState({ profilePicture: e.target.files[0] });
+    }
   };
 
   handleFormSubmit = (e) => {
@@ -100,198 +110,224 @@ export default class ProfileEdit extends Component {
   handlePlaceChange = (place) => {
     this.setState({ location: place.place_name, saved: false });
   };
-
+  handlePortfolio = (index, event) => {
+    this.setState({ saved: false });
+    const portfolio = [...this.state.portfolio];
+    portfolio[index][event.target.name] = event.target.value;
+    this.setState({ portfolio: portfolio });
+  };
   render() {
     return (
-      <form
-        onChange={this.handleFormChange}
-        onSubmit={this.handleFormSubmit}
-        className="profile container"
-      >
-        <button
-          className={
-            this.state.saved
-              ? "edit__button saved"
-              : this.state.usernameAvailable
-              ? "edit__button unsaved"
-              : "edit__button invalid"
-          }
+      <>
+        <form
+          onChange={this.handleFormChange}
+          onSubmit={this.handleFormSubmit}
+          className="profile container"
         >
-          {this.state.saved ? "Saved" : "Save"}
-        </button>
-        {this.state.profilePicture && (
-          <div className="profile__avatarbox">
-            <label htmlFor="profilePicture">
-              <input
-                type="file"
-                name="profilePicture"
-                id="profilePicture"
-                className="input--hidden"
-              />
-              <img
-                className="profile__picture"
-                src={this.state.profilePicture}
-                alt=""
-              />
-            </label>
-          </div>
-        )}
-        <h2 className="profile__name">
-          <input type="text" name="name" id="name" value={this.state.name} />
-        </h2>
-        {this.state.userCategory && (
-          <div className="profile__categories">
-            <Autocomplete
-              multiple
-              onChange={this.handleCategoryChange}
-              limitTags={3}
-              id="tags-outlined"
-              options={this.state.categoryOptions}
-              defaultValue={this.state.userCategory}
-              getOptionLabel={(option) => option.name} // specify what property to use
-              filterSelectedOptions
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </div>
-        )}
-        {this.state.title && (
-          <TextareaAutosize
-            type="text"
-            name="title"
-            id="title"
-            value={this.state.title}
-            className="profile__title"
-            maxLength={280}
-            placeholder="Intro"
-          />
-        )}
-        {this.state.socialLinks && (
-          <div className="profile__sociallinks">
-            {this.state.socialLinks.map((link) => {
-              return (
-                <a href={link} className="profile__sociallink">
-                  {checkLink(link)}
-                </a>
-              );
-            })}
-            <span className="edit--container">
-              <span className="edit--right">edit</span>
-            </span>
-          </div>
-        )}
-        <ul className="profile__bullets">
-          <li className="profile__bullet">
-            Username:{" "}
-            <input
-              type="text"
-              name="userName"
-              id="userName"
-              spellcheck="false"
-              onChange={this.handleUsername}
-              value={this.state.userName}
-            />
-            {!this.state.saved && (this.state.usernameAvailable ? "✅" : "❌")}
-          </li>
-          <li className="profile__bullet">
-            <select
-              id="openToProjects"
-              value={this.state.openToProjects}
-              name="openToProjects"
-            >
-              <option value={true}>Open to collaborations</option>
-              <option value={false}>
-                Not open to collaborations right now
-              </option>
-            </select>
-          </li>
-          <li className="profile__bullet">
-            Based in
-            <CityAutoComplete
-              onSelect={this.handlePlaceChange}
-              userLocation={this.state.location}
-            />
-          </li>
-          <li className="profile__bullet">
-            <select id="remote" value={this.state.remote} name="remote">
-              <option value={true}>Open to remote collabs</option>
-              <option value={false}>Not open to remote collabs</option>
-            </select>
-          </li>
-          <li className="profile__bullet">
-            Preferred method of contact:{" "}
-            <input
-              type="text"
-              name="preferredContact"
-              id="preferredContact"
-              value={this.state.preferredContact || ""}
-              placeholder="Contact"
-            />
-          </li>
-        </ul>
-        {this.state.userSkills && this.state.skillOptions && (
-          <>
-            <h2 className="profile__heading">Skills</h2>
-            <div className="profile__skills">
+          <button
+            className={
+              this.state.saved
+                ? "edit__button saved"
+                : this.state.usernameAvailable
+                ? "edit__button unsaved"
+                : "edit__button invalid"
+            }
+          >
+            {this.state.saved ? "Saved" : "Save"}
+          </button>
+          {this.state.profilePicture && (
+            <div className="profile__avatarbox">
+              <label htmlFor="profilePicture">
+                <input
+                  type="file"
+                  name="profilePicture"
+                  id="profilePicture"
+                  className="input--hidden"
+                />
+                <img
+                  className="profile__picture"
+                  src={this.state.temporaryPicture || this.state.profilePicture}
+                  alt=""
+                />
+              </label>
+            </div>
+          )}
+          <h2 className="profile__name">
+            <input type="text" name="name" id="name" value={this.state.name} />
+          </h2>
+          {this.state.userCategory && (
+            <div className="profile__categories">
               <Autocomplete
                 multiple
-                limitTags={5}
-                onChange={this.handleSkillChange}
+                onChange={this.handleCategoryChange}
+                limitTags={3}
                 id="tags-outlined"
-                options={this.state.skillOptions}
-                defaultValue={this.state.userSkills}
+                options={this.state.categoryOptions}
+                defaultValue={this.state.userCategory}
                 getOptionLabel={(option) => option.name} // specify what property to use
                 filterSelectedOptions
                 renderInput={(params) => <TextField {...params} />}
               />
             </div>
-          </>
-        )}
-        {this.state.bio && (
-          <>
-            <h2 className="profile__heading">About</h2>
+          )}
+          {this.state.title && (
             <TextareaAutosize
-              type="bio"
-              name="bio"
-              id="bio"
-              value={this.state.bio}
-              className="profile__bio"
+              type="text"
+              name="title"
+              id="title"
+              value={this.state.title}
+              className="profile__title"
+              maxLength={280}
               placeholder="Intro"
             />
-          </>
-        )}
+          )}
+          {this.state.socialLinks && (
+            <div className="profile__sociallinks">
+              {this.state.socialLinks.map((link) => {
+                return (
+                  <a href={link} className="profile__sociallink">
+                    {checkLink(link)}
+                  </a>
+                );
+              })}
+              <span className="edit--container">
+                <span className="edit--right">edit</span>
+              </span>
+            </div>
+          )}
+          <ul className="profile__bullets">
+            <li className="profile__bullet">
+              Username:{" "}
+              <input
+                type="text"
+                name="userName"
+                id="userName"
+                spellcheck="false"
+                onChange={this.handleUsername}
+                value={this.state.userName}
+              />
+              {!this.state.saved &&
+                (this.state.usernameAvailable ? "✅" : "❌")}
+            </li>
+            <li className="profile__bullet">
+              <select
+                id="openToProjects"
+                value={this.state.openToProjects}
+                name="openToProjects"
+              >
+                <option value={true}>Open to collaborations</option>
+                <option value={false}>
+                  Not open to collaborations right now
+                </option>
+              </select>
+            </li>
+            <li className="profile__bullet">
+              Based in
+              <CityAutoComplete
+                onSelect={this.handlePlaceChange}
+                userLocation={this.state.location}
+              />
+            </li>
+            <li className="profile__bullet">
+              <select id="remote" value={this.state.remote} name="remote">
+                <option value={true}>Open to remote collabs</option>
+                <option value={false}>Not open to remote collabs</option>
+              </select>
+            </li>
+            <li className="profile__bullet">
+              Preferred method of contact:{" "}
+              <input
+                type="text"
+                name="preferredContact"
+                id="preferredContact"
+                value={this.state.preferredContact || ""}
+                placeholder="Contact"
+              />
+            </li>
+          </ul>
+          {this.state.userSkills && this.state.skillOptions && (
+            <>
+              <h2 className="profile__heading">Skills</h2>
+              <div className="profile__skills">
+                <Autocomplete
+                  multiple
+                  limitTags={5}
+                  onChange={this.handleSkillChange}
+                  id="tags-outlined"
+                  options={this.state.skillOptions}
+                  defaultValue={this.state.userSkills}
+                  getOptionLabel={(option) => option.name} // specify what property to use
+                  filterSelectedOptions
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </div>
+            </>
+          )}
+          {this.state.bio && (
+            <>
+              <h2 className="profile__heading">About</h2>
+              <TextareaAutosize
+                type="bio"
+                name="bio"
+                id="bio"
+                value={this.state.bio}
+                className="profile__bio"
+                placeholder="Intro"
+              />
+            </>
+          )}
+        </form>
         {this.state.portfolio && (
           <>
-            <h2 className="profile__heading">Portfolio</h2>
-            <div className="profile__portfolio">
-              {this.state.portfolio.map((portfolioItem) => {
+            <div className="profile__portfolio container">
+              <h2 className="profile__heading">Portfolio</h2>
+              {this.state.portfolio.map((portfolioItem, index) => {
                 return (
-                  <div className="profile__portfolioitem">
+                  <form
+                    className="profile__portfolioitem"
+                    onChange={(event) => this.handlePortfolio(index, event)}
+                  >
                     <img
                       className="profile__portfolioimage"
                       src={portfolioItem.image}
                       alt=""
                     />
                     <h3 className="profile__portfoliotitle">
-                      {portfolioItem.title}
+                      <input
+                        type="text"
+                        name={"title"}
+                        id={"title"}
+                        value={portfolioItem.title}
+                      />
                     </h3>
-                    <div className="profile__portfoliodescription">
-                      {portfolioItem.description}
+                    <TextareaAutosize
+                      name="description"
+                      id="description"
+                      value={portfolioItem.description}
+                      className="profile__portfoliodescription"
+                      placeholder="Intro"
+                    />
+                    <div className="profile__portfoliolink">
+                      <input
+                        type="text"
+                        name={"link"}
+                        id={"link"}
+                        value={portfolioItem.link}
+                      />
                     </div>
-                    {portfolioItem.link && (
-                      <div className="profile__portfoliolink">
-                        <a href={portfolioItem.link}>Link »</a>
-                      </div>
-                    )}
-                  </div>
+                  </form>
                 );
               })}
             </div>
           </>
         )}
+        {this.state.userCollab && (
+          <h2 className="heading container">Collaborations</h2>
+        )}
         {this.state.userCollab &&
           this.state.userCollab.map((collab) => {
             return (
-              <Link className="card" to={`/collab/${collab._id}`}>
+              <Link className="card container" to={`/collab/${collab._id}`}>
                 <div
                   style={{ backgroundImage: `url(${collab.image})` }}
                   className="card__aside"
@@ -315,7 +351,7 @@ export default class ProfileEdit extends Component {
               </Link>
             );
           })}
-      </form>
+      </>
     );
   }
 }
