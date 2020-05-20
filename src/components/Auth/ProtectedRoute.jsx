9231c12c0f9952e0fa618905e048a0c861b19851
William Hermozo <william.hermozo@protonmail.com>
2020-05-20 16:39:54 +0200

    protected routes comments

--- src/components/Auth/ProtectedRoute.jsx
+++ src/components/Auth/ProtectedRoute.jsx
@@ -6,6 +6,7 @@ const ProtectedRoute = ({ component: Component, context, ...rest }) => {
   if (context.isLoading) {
     return null;
   } else if (context.isLoggedIn) {
+  // } else if (context.isLoggedIn && context.user.userName.includes(/{username}) ) {
     return <Route {...rest} render={(props) => <Component {...props} />} />;
   } else {
     return <Redirect to="/login" />;
