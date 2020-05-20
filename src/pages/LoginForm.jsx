0b1e4424bf5bfb30566169d5bebb91a2a2f6a286
William Hermozo <william.hermozo@protonmail.com>
2020-05-21 00:12:46 +0200

    add form validation on signup

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -2,118 +2,106 @@ import React, { Component } from 'react';
 import UserContext from '../components/Auth/UserContext';
 import { withRouter } from 'react-router-dom';
 import apiHandler from '../api/apiHandler';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 import '../styles/Login.scss';
 
-const emailRegex = RegExp(
-  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-);
-
-const formValid = ({ formErrors, ...rest }) => {
-  let valid = true;
-
-  Object.values(formErrors).forEach((val) => {
-    val.length > 0 && (valid = false);
-  });
-
-  Object.values(rest).forEach((val) => {
-    val === null && (valid = false);
-  });
-
-  return valid;
-};
-
 class Login extends Component {
   static contextType = UserContext;
 
   state = {
     accountExists: false,
-    usernameAvailable: true,
+    usernameAvailable: false,
     name: '',
     userName: '',
     email: '',
     password: '',
-    formErrors: {
-      name: '',
-      userName: '',
-      email: '',
-      password: '',
-    },
+    formError: '',
   };
 
   handleClick = () => {
     this.setState((prevState) => ({ accountExists: !prevState.accountExists }));
   };
 
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    let formErrors = { ...this.state.formErrors };
-
-    switch (name) {
-      case 'name':
-        formErrors.name =
-          value.length < 3 ? 'Name must be at least 3 characters long' : '';
-        break;
-      case 'email':
-        value.length > 3 &&
-          (formErrors.email = emailRegex.test(value)
-            ? ''
-            : 'Please enter a valid email address');
-        break;
-      case 'password':
-        value.length > 2 &&
-          (formErrors.password =
-            value.length < 5
-              ? 'Password should be at least 5 characters long'
-              : '');
-        break;
-      default:
-        break;
-    }
-    this.setState({ [name]: value, formErrors });
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
   };
 
   handleUsername = (e) => {
+    if (
+      !e.target.value.match(/^[a-zA-Z0-9_]{3,10}$/) ||
+      e.target.value.match(
+        /^(about|user|collab|collabs|messages|message|edit|login|signup|freely)$/i
+      )
+    ) {
+      this.setState({ usernameAvailable: false });
+      return;
+    }
     apiHandler.getUser('userName', e.target.value).then((apiRes) => {
-      apiRes.length > 0
-        ? this.setState({ usernameAvailable: false })
-        : this.setState({ usernameAvailable: true });
+      if (apiRes.length > 0) {
+        this.setState({ usernameAvailable: true });
+      } else {
+        this.setState({ usernameAvailable: true });
+      }
     });
+  };
+
+  isValid = () => {
+    let errorMessage = '';
+    if (!this.state.name) {
+      this.setState({ formError: 'You forgot your name!' });
+    } else if (!this.state.userName) {
+      this.setState({ formError: 'You forgot your username!' });
+    } else if (!this.state.email.includes('@')) {
+      this.setState({ formError: 'Please enter a valid email.' });
+    } else if (errorMessage) {
+      this.setState({ formError: errorMessage });
+      return false;
+    }
+    return true;
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    if (formValid(this.state)) {
-      const { email, password, name, userName } = this.state;
-      if (this.state.accountExists) {
-        apiHandler
-          .signin({ email, password })
-          .then((data) => {
-            this.context.setUser(data);
-            this.props.history.push('/');
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      } else {
-        apiHandler
-          .signup({ email, password, name, userName })
-          .then((data) => {
-            this.context.setUser(data);
-            this.props.history.push('/');
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
+    console.log(this.state.formError);
+    if (this.isValid()) {
+      if (this.state.accountExists) return this.logUser();
+      this.createAccount();
     }
   };
 
+  logUser = () => {
+    const { email, password } = this.state;
+    apiHandler
+      .signin({ email, password })
+      .then((data) => {
+        this.context.setUser(data);
+        this.props.history.push('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  createAccount = () => {
+    const { email, password, name, userName } = this.state;
+    apiHandler
+      .signup({ email, password, name, userName })
+      .then((data) => {
+        this.context.setUser(data);
+        this.props.history.push('/');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   render() {
-    const { formErrors } = this.state;
     return (
       <div className="login-container">
         {this.state.accountExists && (
@@ -147,7 +135,6 @@ class Login extends Component {
                   id="name"
                   name="name"
                 />
-                <p className="form-login__error-msg">{formErrors.name}</p>
               </div>
               <div className="form-login__group">
                 <label className="form-login__label" htmlFor="userName">
@@ -161,12 +148,6 @@ class Login extends Component {
                   name="userName"
                   onChange={this.handleUsername}
                 />
-                <p className="form-login__error-msg">{formErrors.userName}</p>
-                {!this.state.usernameAvailable && (
-                  <p className="form-login__error-msg">
-                    Sorry, this username is already taken
-                  </p>
-                )}
               </div>
             </>
           )}
@@ -181,7 +162,6 @@ class Login extends Component {
               id="email"
               name="email"
             />
-            <p className="form-login__error-msg">{formErrors.email}</p>
           </div>
           <div className="form-login__group">
             <label className="form-login__label" htmlFor="password">
@@ -195,7 +175,6 @@ class Login extends Component {
               name="password"
               placeholder={this.state.accountExists ? null : '5+ characters'}
             />
-            <p className="form-login__error-msg">{formErrors.password}</p>
           </div>
           <p className="form-login__error-msg">{this.state.formError}</p>
           <div className="form-login__button-group">
