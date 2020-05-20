6320594e21ad411649b3ad3e7d88e35ab111118e
William Hermozo <william.hermozo@protonmail.com>
2020-05-20 13:55:19 +0200

    add styling to 404 error page

--- src/pages/Error_404.jsx
+++ src/pages/Error_404.jsx
@@ -4,15 +4,17 @@ const Error_404 = () => {
   return (
     <div className="error-page">
       <h1 className="error-page__title">
-        Woooops<br></br><span>four-oh-four</span>
+        Woooops<br></br>
+        <span>four-oh-four</span>
       </h1>
       <img
         src="./media/error_404.svg"
-        alt="error page image"
+        alt="error page palm-tree not-found"
         className="error-page__image"
       />
       <div className="error-page__body">
         <p>wtf did you type in the URL?!</p>
+        <h3>Back home</h3>
       </div>
     </div>
   );
