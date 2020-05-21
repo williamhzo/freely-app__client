bc74427d053f835fad784c3c0a52421bb3e77435
William Hermozo <william.hermozo@protonmail.com>
2020-05-21 11:18:24 +0200

    add route protection on /:username/edit

--- src/components/Auth/ProtectedEditRoute.jsx
+++ src/components/Auth/ProtectedEditRoute.jsx
@@ -1,28 +1,36 @@
 // } else if (context.isLoggedIn && context.user.userName.includes(/{username}) ) {
 
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { withUser } from './withUser';
 
-const ProtectedEditRoute = ({
-  component: Component,
-  context,
-  path,
-  username,
-  ...rest
-}) => {
-  if (context.isLoading) {
-    return null;
-  } else if (
-    context.isLoggedIn
-    // &&
-    // context.user.userName.includes({ path }, -5)
-  ) {
-    return <Route {...rest} render={(props) => <Component {...props} />} />;
-  } else {
-    //add error message: you must be authentified to access this page
-    return <Redirect to="/error_404" />;
-  }
-};
+// component below is redundant: check if performed directly within /:username/edit pages (ProfileEdit.jsx)
 
-export default withUser(ProtectedEditRoute);
+// import React from 'react';
+// import { Redirect, Route, withRouter } from 'react-router-dom';
+// import { withUser } from './withUser';
+
+// const ProtectedEditRoute = ({
+//   component: Component,
+//   context,
+//   path,
+//   username,
+//   history,
+//   location,
+//   match,
+//   // match.params.username
+//   ...rest
+// }) => {
+//   console.log(match)
+//   if (context.isLoading) {
+//     return null;
+//   } else if (
+//     context.isLoggedIn
+//     // &&
+//     // context.user.userName.includes({ path }, -5)
+//   ) {
+//     return <Route {...rest} render={(props) => <Component {...props} />} />;
+//   } else {
+//     //add error message: you must be authentified to access this page
+//     return <Redirect to="/error_404" />;
+//   }
+// };
+
+// export default withRouter(withUser(ProtectedEditRoute));
