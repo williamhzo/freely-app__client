0122a230223f890ce1a6df948ed7b7914247cf5b
William Hermozo <william.hermozo@protonmail.com>
2020-05-24 11:24:18 +0200

    fix font-fam on about us card

--- src/components/Cards/AboutUsCard.jsx
+++ src/components/Cards/AboutUsCard.jsx
@@ -5,11 +5,14 @@ const AboutUsCard = () => {
   return (
     <div className="card card-about">
       <h1 className="about__title card-about__title">
-        Freely <span className="about__title--span card-about-span">is the</span> freelancer
-        platform <span className="about__title--span card-about-span">for</span> freelancers
+        Freely is{' '}
+        <span className="about__title--span card-about-span">the</span>{' '}
+        freelancer platform{' '}
+        <span className="about__title--span card-about-span">for</span>{' '}
+        freelancers
       </h1>
       <Link className="card-about__link" to="/about">
-        How it Works <span className='card-about__chevron'>></span>
+        How it Works <span className="card-about__chevron">></span>
       </Link>
     </div>
   );
